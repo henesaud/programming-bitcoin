9b356b7e081e94eb2faa834510137ae40e639382
henesaud <hstuchisaud@gmail.com>
2024-01-07 19:15:10 -0300

    feat: creating initial field element class

---
+++ src/elements.py
@@ -0,0 +1,21 @@
+class FieldElement:
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = f"Num {num} not in field range 0 to {prime-1}"
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self):
+        return f"FieldElement_{self.prime}({self.num})"
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError("Cannot add two numbers in different Fields")
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)

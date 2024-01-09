aaba8ad1999fb67aa4da61d7e8ee371c9f196150
henesaud <hstuchisaud@gmail.com>
2024-01-08 23:14:56 -0300

    feat: adding remnant opperations

--- src/elements.py
+++ src/elements.py
@@ -9,13 +9,38 @@ class FieldElement:
     def __repr__(self):
         return f"FieldElement_{self.prime}({self.num})"
 
+    def _are_in_same_field(self, other):
+        return self.prime == other.prime
+
     def __eq__(self, other):
         if other is None:
             return False
-        return self.num == other.num and self.prime == other.prime
+        return self.num == other.num and self._are_in_same_field(other)
 
     def __add__(self, other):
-        if self.prime != other.prime:
+        if not self._are_in_same_field(other):
             raise TypeError("Cannot add two numbers in different Fields")
         num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if not self._are_in_same_field(other):
+            raise TypeError("Cannot subtract two numbers in different Fields")
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __mul__(self, other):
+        if not self._are_in_same_field(other):
+            raise TypeError("Cannot multiply two numbers in different Fields")
+        num = (self.num * other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __pow__(self, exponent):
+        num = pow(self.num, exponent, self.prime)
+        return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+        if not self._are_in_same_field(other):
+            raise TypeError("Cannot divide two numbers in different Fields")
+        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
         return self.__class__(num, self.prime)

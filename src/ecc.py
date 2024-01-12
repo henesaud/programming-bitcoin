243cd5fce3d786a87d614d937af9a7f3245b1b9e
henesaud <hstuchisaud@gmail.com>
2024-01-11 22:38:20 -0300

    feat: creating class for point in a elyptic curve

--- src/ecc.py
+++ src/ecc.py
@@ -45,3 +45,51 @@ class FieldElement:
             raise TypeError("Cannot divide two numbers in different Fields")
         num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
         return self.__class__(num, self.prime)
+
+
+class Point:
+    "A point in a elyptcic curve"
+
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+
+        # Accommodates points at infinity. None indicates infinity.
+        if self.x is None and self.y is None:
+            return
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError(f"({x}, {y}) is not on the curve")
+
+    def _are_in_same_curve(self, other):
+        return self.a == other.a and self.b == other.b
+
+    def __eq__(self, other):
+        return (
+            self.x == other.x and self.y == other.y and self._are_in_same_curve(other)
+        )
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __add__(self, other):
+        if not self._are_in_same_curve(other):
+            raise TypeError(f"Points {self}, {other} are not on the same curve")
+
+        if self.x is None:
+            return other
+
+        if other.x is None:
+            return self
+
+        # Points are additive inverses
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
